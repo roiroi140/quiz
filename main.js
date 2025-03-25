c9ed3341553f5ca56bd850cd278b99675861aca9
roiroi140 <mzkrkk@icloud.com>
2025-03-25 11:03:26 +0900

    iroiro

--- main.js
+++
@@ -1,8 +0,0 @@
-function toggleAnswer(id) {
-    const answer = document.getElementById(id);
-    const button = answer.previousElementSibling;
-    const isVisible = answer.style.display === "block";
-
-    answer.style.display = isVisible ? "none" : "block";
-    button.setAttribute("aria-expanded", !isVisible);
-}
